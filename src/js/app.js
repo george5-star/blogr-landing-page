1ef73156b7ad1bccb40298cc0de0d6e6bfc9ecf7
George Asiedu <georgeasiedu543@gmail.com>
2023-11-19 21:21:55 +0000

    Add mobile navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,25 @@
 "use strict";
 
-console.log("hello world");
+const hamburgerEl = document.querySelector(".hamburger");
+const closeEl = document.querySelector(".close");
+const navBar = document.querySelector(".mobile-wrapper");
+const navLists = document.querySelectorAll(".nav-list__item");
+
+hamburgerEl.addEventListener("click", () => {
+  hamburgerEl.classList.add("hide");
+  closeEl.classList.add("show");
+  navBar.classList.add("show");
+});
+
+closeEl.addEventListener("click", () => {
+  closeEl.classList.remove("show");
+  navBar.classList.remove("show");
+  hamburgerEl.classList.remove("hide");
+});
+
+navLists.forEach((navListItem) => {
+  navListItem.addEventListener("click", function (e) {
+    const navSubList = e.target.parentElement.children[1];
+    navSubList.classList.toggle("show");
+  });
+});
